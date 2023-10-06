3196cc0fc9bac66b3abf00a3e01c8c61b74476ee
motiullahsajit <sajitmotiullah@gmail.com>
2023-10-06 18:27:40 +0600

    dashboard added

---
+++ admin-contrri-vista-client/src/views/rtl/default/index.jsx
@@ -0,0 +1,102 @@
+import MiniCalendar from "components/calendar/MiniCalendar";
+import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
+import TotalSpent from "views/admin/default/components/TotalSpent";
+import PieChartCard from "views/admin/default/components/PieChartCard";
+import { IoMdHome } from "react-icons/io";
+import { IoDocuments } from "react-icons/io5";
+import { MdBarChart, MdDashboard } from "react-icons/md";
+
+import { columnsDataCheck, columnsDataComplex } from "./variables/columnsData";
+
+import Widget from "views/rtl/default/components/Widget";
+import CheckTable from "views/rtl/default/components/CheckTable";
+import ComplexTable from "views/rtl/default/components/ComplexTable";
+import DailyTraffic from "views/rtl/default/components/DailyTraffic";
+import TaskCard from "views/rtl/default/components/TaskCard";
+import tableDataCheck from "./variables/tableDataCheck.json";
+import tableDataComplex from "./variables/tableDataComplex.json";
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* Card widget */}
+
+      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Active Users"}
+          subtitle={"340"}
+        />
+        <Widget
+          icon={<IoDocuments className="h-6 w-6" />}
+          title={"SignUps this month"}
+          subtitle={"642"}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Projects this month"}
+          subtitle={"34"}
+        />
+        <Widget
+          icon={<MdDashboard className="h-6 w-6" />}
+          title={"Completed this month"}
+          subtitle={"10"}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"New Tasks"}
+          subtitle={"145"}
+        />
+        <Widget
+          icon={<IoMdHome className="h-6 w-6" />}
+          title={"Total Projects"}
+          subtitle={"2433"}
+        />
+      </div>
+
+      {/* Charts */}
+
+      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
+        <TotalSpent />
+        <WeeklyRevenue />
+      </div>
+
+      {/* Tables & Charts */}
+
+      <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
+        {/* Check Table */}
+        <div>
+          <CheckTable
+            columnsData={columnsDataCheck}
+            tableData={tableDataCheck}
+          />
+        </div>
+
+        {/* Traffic chart & Pie Chart */}
+
+        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
+          <DailyTraffic />
+          <PieChartCard />
+        </div>
+
+        {/* Complex Table , Task & Calendar */}
+
+        <ComplexTable
+          columnsData={columnsDataComplex}
+          tableData={tableDataComplex}
+        />
+
+        {/* Task chart & Calendar */}
+
+        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
+          <TaskCard />
+          <div className="grid grid-cols-1 rounded-[20px]">
+            <MiniCalendar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
